5bbfa8c5fe256c15a2a94e636fd33c3711e34695
anonymousip1234 <89058595+anonymousip1234@users.noreply.github.com>
2022-06-11 18:46:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Logical_programming
+{
+
+    class Program
+    {
+
+        public void Main(string[]args)
+        {
+            int n;
+            Console.WriteLine("Welcome To Day 6 of programming,Choose from the following numbers");
+            Console.WriteLine("1.Fibonacci series");
+            Console.WriteLine("2.Prime number");
+            Console.WriteLine("3.Perfect number");
+            Console.WriteLine("4.Reverse a number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            switch(n)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Series();
+                    break;
+
+                case 2:
+                    Prime prime = new Prime();
+                    prime.PRIME();
+                    break;
+
+
+                case 3:
+                    Perfect_number perfect_Number = new Perfect_number();
+                    perfect_Number.PERFECT();
+                    break;
+
+
+
+                case 4:
+                    Reverse reverse = new Reverse();
+                    reverse.REVERSE();
+                    break;
+
+
+
+
+
+                    
+
+            
+
+            }
+
+        }
+
+
+    }
+
+
+}
